bcb27f96b38c7abeae8eafaa686cdf7f3b8fbd2d
jd.rabeb <jd.rabeb@gmail.com>
2019-07-05 00:25:19 +0200

    move base_url to trig()

--- src/ifttt.py
+++ src/ifttt.py
@@ -1,11 +1,10 @@
 import requests
 
 class ifttt_webhooks:
-    base_url = 'https://maker.ifttt.com/trigger/{}/with/key/{}'
-
-    def __init__(self, key):
+        def __init__(self, key):
         self.key = key
 
     def trig(self, event_name):
+        base_url = 'https://maker.ifttt.com/trigger/{}/with/key/{}'
         url = base_url.format(event_name, self.key)
         r = requests.get(url)
